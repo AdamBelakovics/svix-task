5d1a07f778059701591b90a10ae8773665b2362e
Adam Belakovics <beladi96@gmail.com>
2023-03-30 16:06:29 +0200

    Add dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { EventTypePage } from "./event-types/EventTypePage";
+
 function App() {
-  return <p>Hello world</p>;
+  return (
+    <ChakraProvider>
+      <EventTypePage />
+    </ChakraProvider>
+  );
 }
 
 export default App;
---
+++ src/event-types/EventTypePage.tsx
@@ -0,0 +1,3 @@
+export function EventTypePage() {
+  return <h1>Hello</h1>;
+}
