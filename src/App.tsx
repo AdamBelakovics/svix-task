204674a740bcd0cc76203ea25cf4d13b32cf7332
Adam Belakovics <beladi96@gmail.com>
2023-03-30 18:11:35 +0200

    Add simple global error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { toast, Toaster } from "react-hot-toast";
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
 import { Svix } from "svix";
 import { EventTypePage } from "./event-types/EventTypePage";
 import { SvixClientContext } from "./utils/svixClient.context";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  mutationCache: new MutationCache({
+    onError: (error: any) => {
+      toast.error(`Something went wrong: ${error?.message}`);
+    },
+  }),
+  queryCache: new QueryCache({
+    onError: (error: any) => {
+      toast.error(`Something went wrong: ${error?.message}`);
+    },
+  }),
+});
 
 const svixClient = new Svix(process.env.REACT_APP_SVIX_API_KEY as string);
 
@@ -14,6 +31,7 @@ function App() {
       <ChakraProvider>
         <SvixClientContext.Provider value={svixClient}>
           <EventTypePage />
+          <Toaster />
         </SvixClientContext.Provider>
       </ChakraProvider>
     </QueryClientProvider>
